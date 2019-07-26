515151fdf6b9b978c39b11063f8411df58b10690
flash6199 <flash619+bitbucket@pm.me>
2019-07-26 10:00:00 -0500

    Updated dependancies. Added experimental feature for canceling requests within the onRequest middleware function.

--- src/epics.ts
+++ src/epics.ts
@@ -1,9 +1,14 @@
-import {Epic} from "redux-observable";
-import {catchError, filter, map, mergeMap, tap} from "rxjs/operators";
-import {generateAjaxRequest, generateRequestAbortAction, generateRequestResponseAction, isRequest} from "./utilities";
-import {of} from "rxjs";
-import {RequestAction, RequestSubscriberOptions} from "./index";
-import {AjaxResponse} from "rxjs/ajax";
+import { Epic } from "redux-observable";
+import { catchError, filter, map, mergeMap, tap } from "rxjs/operators";
+import {
+  generateAjaxRequest,
+  generateRequestAbortAction,
+  generateRequestResponseAction,
+  isRequest
+} from "./utilities";
+import { of } from "rxjs";
+import { RequestAction, RequestSubscriberOptions } from "./index";
+import { AjaxResponse } from "rxjs/ajax";
 
 //-----------------------------------------------------------------
 // Active/Pending request tracing.
@@ -11,7 +16,8 @@ import {AjaxResponse} from "rxjs/ajax";
 
 const activeActions: string[] = [];
 
-const removeActiveAction = (action: string) => activeActions.splice(activeActions.indexOf(action), 1);
+const removeActiveAction = (action: string) =>
+  activeActions.splice(activeActions.indexOf(action), 1);
 const addActiveAction = (action: string) => activeActions.push(action);
 const isActiveAction = (action: string) => activeActions.indexOf(action) !== -1;
 
@@ -19,61 +25,68 @@ const isActiveAction = (action: string) => activeActions.indexOf(action) !== -1;
 // High order epic generation.
 //-----------------------------------------------------------------
 
-export const generateRequestSubscriber = (options?: RequestSubscriberOptions) => {
+export const generateRequestSubscriber = (
+  options?: RequestSubscriberOptions
+) => {
+  // Request/Response Handlers
 
-    // Request/Response Handlers
+  const handleRequest = (action: RequestAction) => {
+    if (options && options.onRequest != null) {
+      return options.onRequest(action);
+    }
+    return action;
+  };
 
-    const handleRequest = (action: RequestAction) => {
-        if (options && options.onRequest != null) {
-            return options.onRequest(action)
+  const handleAbort = (action: RequestAction) => {
+    let nextAction = generateRequestAbortAction(action);
+    if (options != null && options.onAbort != null) {
+      nextAction = options.onAbort(nextAction);
+    }
+    return nextAction;
+  };
+
+  const handleSuccess = (
+    action: RequestAction,
+    response: AjaxResponse
+  ): object => {
+    let nextAction = generateRequestResponseAction(action, response);
+    if (options != null && options.onSuccess != null) {
+      nextAction = options.onSuccess(nextAction);
+    }
+    removeActiveAction(action.type);
+    return nextAction;
+  };
+
+  const handleError = (action: RequestAction, error: AjaxResponse) => {
+    let nextAction = generateRequestResponseAction(action, error);
+    if (options != null && options.onError != null) {
+      nextAction = options.onError(nextAction);
+    }
+    removeActiveAction(action.type);
+    return nextAction;
+  };
+
+  // Epic / RXJS
+
+  const requestSubscriber: Epic<any> = action$ =>
+    action$.pipe(
+      filter(action => isRequest(action)),
+      mergeMap(action => {
+        if (isActiveAction(action.type)) {
+          return of(handleAbort(action));
+        } else {
+          addActiveAction(action.type);
+          action = handleRequest(action);
+          if (action == null) {
+            return of({});
+          }
+          return generateAjaxRequest(action).pipe(
+            map(response => handleSuccess(action, response)),
+            catchError(error => of(handleError(action, error)))
+          );
         }
-        return action
-    };
-
-    const handleAbort = (action: RequestAction) => {
-        let nextAction = generateRequestAbortAction(action);
-        if (options != null && options.onAbort != null) {
-            nextAction = options.onAbort(nextAction)
-        }
-        return nextAction
-    };
-
-    const handleSuccess = (action: RequestAction, response: AjaxResponse) => {
-        let nextAction = generateRequestResponseAction(action, response);
-        if (options != null && options.onSuccess != null) {
-            nextAction = options.onSuccess(nextAction)
-        }
-        removeActiveAction(action.type);
-        return nextAction
-    };
-
-    const handleError = (action: RequestAction, error: AjaxResponse) => {
-        let nextAction = generateRequestResponseAction(action, error);
-        if (options != null && options.onError != null) {
-            nextAction = options.onError(nextAction)
-        }
-        removeActiveAction(action.type);
-        return nextAction
-    };
-
-    // Epic / RXJS
-
-    const requestSubscriber: Epic<any> = action$ => action$.pipe(
-        filter(action => isRequest(action)),
-        mergeMap(action => {
-                if (isActiveAction(action.type)) {
-                    return of(handleAbort(action));
-                } else {
-                    addActiveAction(action.type);
-                    action = handleRequest(action);
-                    return generateAjaxRequest(action).pipe(
-                        map(response => handleSuccess(action, response)),
-                        catchError(error => of(handleError(action, error)))
-                    )
-                }
-            }
-        )
+      })
     );
 
-    return requestSubscriber
+  return requestSubscriber;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
-import {AjaxError, AjaxRequest, AjaxResponse} from "rxjs/ajax";
+import { AjaxError, AjaxRequest, AjaxResponse } from "rxjs/ajax";
 
-export * from './utilities'
-export * from './epics'
+export * from "./utilities";
+export * from "./epics";
 
 export interface RequestSubscriberOptions {
-   onRequest?: (requestAction: RequestAction) => RequestAction
-   onSuccess?: (responseAction: ResponseAction) => ResponseAction
-   onError?: (responseAction: ResponseAction) => ResponseAction
-   onAbort?: (responseAction: ResponseAction) => ResponseAction
+  onRequest?: (requestAction: RequestAction) => RequestAction | null;
+  onSuccess?: (responseAction: ResponseAction) => ResponseAction;
+  onError?: (responseAction: ResponseAction) => ResponseAction;
+  onAbort?: (responseAction: ResponseAction) => ResponseAction;
 }
 
 export interface RequestAction {
-    type: string
-    request?: AjaxRequest
-    meta?: {[key:string]:any}
-    payload?:{
-        request?:AjaxRequest
-    }
+  type: string;
+  request?: AjaxRequest;
+  meta?: { [key: string]: any };
+  payload?: {
+    request?: AjaxRequest;
+  };
 }
 
-export interface ResponseAction{
-    type: string
-    meta: {
-        [key:string]:any
-        originalAction: RequestAction
-        ajaxResponse?: AjaxResponse|AjaxError
-    }
-    response?: AjaxResponse["response"]|AjaxError["response"]
-    payload?: {
-        response:AjaxResponse["response"]|AjaxError["response"]
-    }
+export interface ResponseAction {
+  type: string;
+  meta: {
+    [key: string]: any;
+    originalAction: RequestAction;
+    ajaxResponse?: AjaxResponse | AjaxError;
+  };
+  response?: AjaxResponse["response"] | AjaxError["response"];
+  payload?: {
+    response: AjaxResponse["response"] | AjaxError["response"];
+  };
 }
